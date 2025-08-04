29ba304750c012fb6ac028be466721f652c679c0
Steven Zvonek <6392944+steven1046@users.noreply.github.com>
2025-08-04 11:00:05 -0500

    Update dojo/dijit to 1.13.1

--- dev/com.ibm.ws.ui/lib/releaseProfile.js
+++ dev/com.ibm.ws.ui/lib/releaseProfile.js
@@ -4,14 +4,14 @@
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 
-var dojoVersion = '1.12.11';
+var dojoVersion = '1.13.10';
 var gridxVersion = '1.3.9';
 var idxVersion = '1.5.2.6';
 
