6ae603ba37cbcd1a4026fd3d3097349bbe0a19d8
Benjamin Confino <benjamic@uk.ibm.com>
2025-08-25 21:39:42 +0100

    TCK is running but not passing

---
+++ dev/com.ibm.ws.jpa.container.v32_fat_tck/publish/JPAContainerV32Launcher.java
@@ -0,0 +1,84 @@
+/*******************************************************************************
+ * Copyright (c) 2018, 2024 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package io.openliberty.jpaContainer.v32.tck;
+
+import java.util.Collections;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import componenttest.annotation.AllowedFFDC;
+import componenttest.annotation.Server;
+import componenttest.custom.junit.runner.FATRunner;
+import componenttest.custom.junit.runner.Mode.TestMode;
+import componenttest.custom.junit.runner.TestModeFilter;
+import componenttest.topology.impl.LibertyServer;
+import componenttest.topology.utils.tck.TCKResultsInfo.Type;
+import componenttest.topology.utils.tck.TCKRunner;
+
+/**
+ * This is a test class that runs the whole Fault Tolerance TCK. The TCK results
+ * are copied in the results/junit directory before the Simplicity FAT framework
+ * generates the html report - so there is detailed information on individual
+ * tests as if they were running as simplicity junit FAT tests in the standard
+ * location.
+ */
+@RunWith(FATRunner.class)
+public class JPAContainerV32Launcher {
+
+    private static final String SERVER_NAME = "JPATCKServer";
+
+    private static final boolean FAT_TEST_LOCALRUN = Boolean.getBoolean("fat.test.localrun");
+
+    @Server(SERVER_NAME)
+    public static LibertyServer server;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        server.startServer();
+    }
+
+    /**
+     * Various TCK tests test for Deployment, Definition and other Exceptions and
+     * these will cause the test suite to be marked as FAILED if found in the logs
+     * when the server is shut down. So we tell Simplicity to allow for the message
+     * ID's below.
+     *
+     * @throws Exception
+     */
+    @AfterClass
+    public static void tearDown() throws Exception {
+        server.stopServer();
+    }
+
+    /**
+     * Run the TCK (controlled by autoFVT/publish/tckRunner/tcl/tck-suite.html)
+     *
+     * @throws Exception
+     */
+    @Test
+    @AllowedFFDC // The tested exceptions cause FFDC so we have to allow for this.
+    public void launchFaultTolerance40TCK() throws Exception {
+        boolean isFullMode = TestModeFilter.shouldRun(TestMode.FULL);
+
+        String suiteFileName = isFullMode ? "tck-suite.xml" : "tck-suite-lite.xml";
+
+        TCKRunner.build(server, Type.JAKARTA, "JPA")
+                        .withSuiteFileName(suiteFileName)
+                        .withLogging(Collections.EMPTY_MAP)
+                        .runTCK();
+    }
+
+}
