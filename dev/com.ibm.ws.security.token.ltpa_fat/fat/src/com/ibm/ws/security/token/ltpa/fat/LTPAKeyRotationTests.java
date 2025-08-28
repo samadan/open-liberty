8d5cf6416717f4908aa4c1ca78b2b988002bb0cd
Karel Harjono <karel.harjono@ibm.com>
2025-08-28 11:03:23 -0400

    update ltpa test for v1 fips key rotation .noFips

--- dev/com.ibm.ws.security.token.ltpa_fat/fat/src/com/ibm/ws/security/token/ltpa/fat/LTPAKeyRotationTests.java
+++ dev/com.ibm.ws.security.token.ltpa_fat/fat/src/com/ibm/ws/security/token/ltpa/fat/LTPAKeyRotationTests.java
@@ -121,6 +121,7 @@ public class LTPAKeyRotationTests {
     private static final String BAD_PUBLIC_VALIDATION_KEY2_PATH = "resources/security/validation8.keys";
 
     // Define the paths to the alternate key files
+    private static String ALT_PRIMARY_KEY_PATH = "alternate/ltpa.keys";
     private static String ALT_VALIDATION_KEY1_PATH = "alternate/validation1.keys";
     private static String ALT_VALIDATION_KEY2_PATH = "alternate/validation2.keys";
     private static String ALT_VALIDATION_KEY3_PATH = "alternate/validation3.keys";
@@ -302,6 +303,51 @@ public class LTPAKeyRotationTests {
             flClient1.releaseClient();
             flClient2.releaseClient();
         }
+    }
+
+    @Test
+    @CheckForLeakedPasswords({ validPassword })
+    @AllowedFFDC({ "java.lang.IllegalArgumentException" })
+    public void testLTPAFileRegeneration_regenerateV1KeysWhenFipsIsEnabled_notRegenerateV1KeysWhenFipsDisabled() throws Exception {
+        if (!fips140_3Enabled) {
+            // ltpa is ver1 on startup since fips is NOT enabled
+            configureServer("true", "10", true);
+            verifyLTPAKeyVersion(DEFAULT_KEY_PATH, "1.0");
+
+            // should NOT regenerate ltpa to fips-compatible keys
+            assertFileWasNotCreated(DEFAULT_KEY_PATH + ".noFips");
+            assertFileWasNotCreated(DEFAULT_KEY_PATH + ".noFips.1");
+        } else {
+            // ltpa is ver2 on startup since fips is enabled
+            configureServer("true", "10", true); 
+            verifyLTPAKeyVersion(DEFAULT_KEY_PATH, "2.0");
+
+            // copy version1 (non-fips) ltpa to server
+            copyFileToServerResourcesSecurityDir(ALT_PRIMARY_KEY_PATH);
+            verifyLTPAKeyVersion(DEFAULT_KEY_PATH, "1.0");
+
+            waitForLTPAConfigurationReadyMessage();
+
+            // should regenerate ltpa to fips-compatible keys
+            assertFileWasCreated(DEFAULT_KEY_PATH + ".noFips");
+
+            copyFileToServerResourcesSecurityDir(ALT_PRIMARY_KEY_PATH);
+            verifyLTPAKeyVersion(DEFAULT_KEY_PATH, "1.0");
+
+            waitForLTPAConfigurationReadyMessage();
+
+            // should regenerate ltpa to fips-compatible keys with .noFips.1 suffix since .noFips already exists
+            assertFileWasCreated(DEFAULT_KEY_PATH + ".noFips.1");
+
+            // verify version 2 keys was generated
+            assertFileWasCreated(DEFAULT_KEY_PATH);
+            verifyLTPAKeyVersion(DEFAULT_KEY_PATH, "2.0");
+        }
+
+        flClient1.accessProtectedServletWithAuthorizedCredentials(FormLoginClient.PROTECTED_SIMPLE, validUser, validPassword);
+
+        String cookie1 = flClient1.getCookieFromLastLogin();
+        assertNotNull("Expected SSO Cookie 1 is missing.", cookie1);
     }
 
     /**
@@ -2614,7 +2660,6 @@ public class LTPAKeyRotationTests {
             Log.info(thisClass, "notifyFileChangesWithMbean", "FileNotificationMBean is not registered.");
             throw new Exception("FileNotificationMBean is not registered.");
         }
-
     }
 
     private static boolean isWindows() {
@@ -2631,4 +2676,34 @@ public class LTPAKeyRotationTests {
         assertNotNull("Expected LTPA configuration ready message not found in the log.", server.waitForStringInLog("CWWKS4105I", timeoutMillis));
     }
 
+    /**
+     * Verify that the LTPA key file has the expected version
+     * 
+     * @param filePath Path to the LTPA key file
+     * @param expectedVersion Expected version string (e.g., "1.0" or "2.0")
+     * @throws Exception
+     */
+    private void verifyLTPAKeyVersion(String filePath, String expectedVersion) throws Exception {
+        Log.info(thisClass, "verifyLTPAKeyVersion", "Verifying LTPA key version in: " + filePath);
+        
+        String absolutePath = server.getServerRoot() + "/" + filePath;
+        boolean versionFound = false;
+        boolean correctVersion = false;
+        
+        try (BufferedReader reader = new BufferedReader(new FileReader(absolutePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("com.ibm.websphere.ltpa.version=")) {
+                    versionFound = true;
+                    String version = line.substring("com.ibm.websphere.ltpa.version=".length());
+                    Log.info(thisClass, "verifyLTPAKeyVersion", "Found version: " + version);
+                    correctVersion = expectedVersion.equals(version);
+                    break;
+                }
+            }
+        }
+        
+        assertTrue("LTPA key version not found in file: " + filePath, versionFound);
+        assertTrue("LTPA key version is not " + expectedVersion + " in file: " + filePath, correctVersion);
+    }
 }
