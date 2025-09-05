4c272b88146456f5c1b6a36ef361743d50ee37f6
Andrew Rouse <anrouse@uk.ibm.com>
2025-09-05 10:54:33 +0100

    MCP: Fix MessageParsingTest
    Pass correct arguments when creating the test ArgumentMetadata

--- dev/io.openliberty.mcp.internal/test/io/openliberty/mcp/internal/test/MessageParsingTest.java
+++ dev/io.openliberty.mcp.internal/test/io/openliberty/mcp/internal/test/MessageParsingTest.java
@@ -48,16 +48,16 @@ public class MessageParsingTest {
         ToolRegistry.set(registry);
 
         Tool testTool = Literals.tool("echo", "Echo", "Echos the input");
-        Map<String, ArgumentMetadata> arguments = Map.of("input", new ArgumentMetadata(String.class, 0, ""));
+        Map<String, ArgumentMetadata> arguments = Map.of("input", new ArgumentMetadata(String.class, 0, "", true));
         registry.addTool(ToolMetadataUtil.createToolMetadataFrom(testTool, arguments));
 
         Tool addTestTool = Literals.tool("add", "Add", "Addition calculator");
-        Map<String, ArgumentMetadata> additionArgs = Map.of("num1", new ArgumentMetadata(Integer.class, 0, ""),
-                                                            "num2", new ArgumentMetadata(Integer.class, 1, ""));
+        Map<String, ArgumentMetadata> additionArgs = Map.of("num1", new ArgumentMetadata(Integer.class, 0, "", true),
+                                                            "num2", new ArgumentMetadata(Integer.class, 1, "", true));
         registry.addTool(ToolMetadataUtil.createToolMetadataFrom(addTestTool, additionArgs));
 
         Tool toogleTestTool = Literals.tool("toggle", "Toggle", "Toggle a boolean");
-        Map<String, ArgumentMetadata> booleanArgs = Map.of("input", new ArgumentMetadata(Boolean.class, 0, "boolean value"));
+        Map<String, ArgumentMetadata> booleanArgs = Map.of("input", new ArgumentMetadata(Boolean.class, 0, "boolean value", true));
         registry.addTool(ToolMetadataUtil.createToolMetadataFrom(toogleTestTool, booleanArgs));
     }
 
