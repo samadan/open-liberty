764a15f408d48592b4a00fd1a915993522d72334
Kyle Aure <KyleJAure@gmail.com>
2025-09-15 11:36:10 -0500

    fix: db2 update image to correctly handle user and kerberos initialization

--- dev/io.openliberty.org.testcontainers/resources/openliberty/testcontainers/db2-krb5/12.1.2.0/custom/createschema.sh
+++ dev/io.openliberty.org.testcontainers/resources/openliberty/testcontainers/db2-krb5/12.1.2.0/custom/createschema.sh
@@ -1,41 +1,51 @@
 #!/bin/sh
 
-source ${SETUPDIR?}/include/db2_constants
-source ${SETUPDIR?}/include/db2_common_functions
+echo "(*) Verify user is root"
+whoami
 
-echo "(*) Verify hostname"
+echo "(*) Change external hostname to db2"
 sudo hostname db2
 hostname
 
+echo "(*) Create new user dbuser"
+useradd -s /bin/bash dbuser
+
+echo "(*) Persist env variables to user profile"
+USERPROFILE=/database/config/db2inst1/sqllib/userprofile
+echo "export DB2_KRB5_PRINCIPAL=${DB2_KRB5_PRINCIPAL}" >> $USERPROFILE
+cat $USERPROFILE
+
+# Execute as database user
 sudo -i -u db2inst1 bash << EOF
 
 echo "(*) Configure DB2 database manager to use Kerberos"
-db2 UPDATE DATABASE MANAGER CONFIGURATION USING CLNT_KRB_PLUGIN IBMkrb5
-db2 UPDATE DATABASE MANAGER CONFIGURATION USING AUTHENTICATION KERBEROS
+db2 update dbm cfg using CLNT_KRB_PLUGIN IBMkrb5
+db2 update dbm cfg using AUTHENTICATION KERBEROS
 
-echo "(*) Log database manager configuration"
-db2 GET DATABASE MANAGER CONFIGURATION
+echo "(*) Debug keytab file"
+klist -k -t /etc/krb5.keytab
 
-echo "(*) Initialize principal ${DB2INSTANCE?}"
-touch /tmp/kinit.log && export KRB5_TRACE=/tmp/kinit.log
-kinit -k -t /etc/krb5.keytab -c FILE:/tmp/krb5cc_1000 ${DB2INSTANCE?}
-cat /tmp/kinit.log
+echo "(*) Initialize kerberos user db2inst1/db2"
+kinit -k -t /etc/krb5.keytab db2inst1/db2
 
-echo "(*) Debug credential cache at /tmp/krb5cc_1000"
-ls -la /tmp/krb5cc_1000
-klist /tmp/krb5cc_1000
+echo "(*) Debug credential cache"
+klist -e -f -a
 
-echo "(*) Debug keytab file at /etc/krb5.keytab"
-ls -la /etc/krb5.keytab
-klist -k /etc/krb5.keytab
+echo "(*) Grant connect to dbuser"
+db2 connect to TESTDB
+db2 grant createtab, implicit_schema, connect on database to user dbuser
+db2 grant use of tablespace USERSPACE1 to user dbuser
+db2 terminate
 
-echo "(*) Start database manager"
-db2start
+echo "(*) Restart database"
 
-echo "(*) Restart DB2"
+$DB2_ADMIN/adm/db2start
 db2 db2stop
 db2 db2start
 
-EOF
+echo "(*) Log database manager configuration"
+db2 get database manager configuration | grep -e KRB -e KERBEROS
 
 echo "SETUP SCRIPT COMPLETE"
+
+EOF
