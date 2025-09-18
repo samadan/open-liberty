eb9acb0a480d575966baa8c227e842d108e0be7c
Neena Jacob <neena.jacob@ibm.com>
2025-09-18 14:35:38 +0530

    Added copyright information

--- dev/io.openliberty.org.jboss.resteasy.common.ee10/src/org/jboss/resteasy/client/jaxrs/internal/proxy/ClientInvoker.java
+++ dev/io.openliberty.org.jboss.resteasy.common.ee10/src/org/jboss/resteasy/client/jaxrs/internal/proxy/ClientInvoker.java
@@ -1,3 +1,21 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ *
+ * Copyright 2025 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jboss.resteasy.client.jaxrs.internal.proxy;
 
 import java.lang.reflect.Method;
@@ -122,9 +140,10 @@ public class ClientInvoker implements MethodInvoker {
         Object e = request.getEntity();
         Object o = null;
         if (e != null) {
+            //Liberty change: Converted e to GenericEntity to fix the issue: 
             o = rxInvoker.method(getHttpMethod(),
                     Entity.entity(new GenericEntity<Object>(e, request.getEntityGenericType()), 
-                    request.getHeaders().getMediaType(), request.getEntityAnnotations()), gt);
+                    request.getHeaders().getMediaType(), request.getEntityAnnotations()), gt); 
         } else {
             o = rxInvoker.method(getHttpMethod(), gt);
         }
