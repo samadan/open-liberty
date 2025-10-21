9702ea7c7544fd127eaf53923b247842d703fc8e
Neena Jacob <neena.jacob@ibm.com>
2025-10-21 13:34:14 +0530

    Implemented review comments

--- dev/io.openliberty.org.jboss.resteasy.cdi.ee11/src/org/jboss/resteasy/cdi/ResteasyCdiExtension.java
+++ dev/io.openliberty.org.jboss.resteasy.cdi.ee11/src/org/jboss/resteasy/cdi/ResteasyCdiExtension.java
@@ -25,8 +25,6 @@ import java.lang.reflect.Member;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.lang.reflect.Type;
-import java.security.AccessController;
-import java.security.PrivilegedAction;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
